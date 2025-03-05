84f885ed8972638b8e984f170a01bef244930239
Lovmong <1967471283@qq.com>
2025-03-05 16:52:16 +0800

    Add RC522 module and examples

---
+++ examples/modules_examples/rc522_read.py
@@ -0,0 +1,13 @@
+from fusion_hat import RC522
+
+rc = RC522()
+rc.Pcd_start()
+print("Reading...Please place the card...")
+
+
+try:
+    message = rc.read(2)
+    print("Successfully retrieved data block:", message)
+    input("Press enter to exit...")
+except KeyboardInterrupt:
+    print("Exiting...")
---
+++ examples/modules_examples/rc522_write.py
@@ -0,0 +1,15 @@
+from fusion_hat import RC522
+
+rc = RC522()
+rc.Pcd_start()
+x = input("Please enter the data to be written:")
+print("Reading...Please place the card...")
+data = [ord(x[i]) for i in range(len(x))]
+
+try:
+    rc.write(2,data)
+    message = rc.read(2)
+    print("Successfully retrieved data block:", message)
+    input("Press enter to exit...")
+except:
+    print("Error")
