9712095bbff165380ac96afd0c6accb1e679e0af
Cavon <cavonxx@gmail.com>
2025-10-17 14:35:39 +0800

    refactor(config): add type hints and docstrings to Config class
    Add type hints and comprehensive docstrings to all methods in the Config class to improve code maintainability and developer experience. The changes include:
    - Adding type annotations for parameters and return values
    - Documenting each method's purpose, parameters, and return values
    - Maintaining existing functionality while improving code clarity

--- fusion_hat/config.py
+++ fusion_hat/config.py
@@ -1,8 +1,16 @@
 import json
 import os
+from typing import Optional, Iterator
+
 
 class Config():
-    def __init__(self, config_file):
+    def __init__(self, config_file: str) -> None:
+        """
+        Initialize the config class
+
+        :param config_file: config file path
+        :type config_file: str
+        """
         self.config_file = config_file
         
         if not os.path.exists(config_file):
@@ -14,40 +22,124 @@ class Config():
                 content = '{}'
             self._config = json.loads(content)
 
-    def get(self, key, default_value=None):
+    def get(self, key: str, default_value: Optional[any] = None) -> any:
+        """
+        Get the value of the key
+
+        :param key: key name
+        :type key: str
+
+        :param default_value: default value if the key is not found
+        :type default_value: Optional[any]
+
+        :return: value of the key
+        :rtype: any
+        """
         return self._config.get(key, default_value)
 
-    def set(self, key, value):
+    def set(self, key: str, value: any) -> None:
+        """
+        Set the value of the key
+
+        :param key: key name
+        :type key: str
+
+        :param value: value of the key
+        :type value: any
+        """
         self._config[key] = value
         with open(self.config_file, 'w') as f:
             json.dump(self._config, f, indent=4)
 
-    def delete(self, key):
+    def delete(self, key: str) -> None:
+        """
+        Delete the key
+
+        :param key: key name
+        :type key: str
+        """
         if key in self._config:
             del self._config[key]
             with open(self.config_file, 'w') as f:
                 json.dump(self._config, f, indent=4)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> any:
+        """
+        Get the value of the key
+
+        :param key: key name
+        :type key: str
+
+        :return: value of the key
+        :rtype: any
+        """
         return self.get(key)
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: str, value: any) -> None:
+        """
+        Set the value of the key
+
+        :param key: key name
+        :type key: str
+
+        :param value: value of the key
+        :type value: any
+        """
         self.set(key, value)
 
-    def __delitem__(self, key):
+    def __delitem__(self, key: str) -> None:
+        """
+        Delete the key
+
+        :param key: key name
+        :type key: str
+        """
         self.delete(key)
 
-    def __contains__(self, key):
+    def __contains__(self, key: str) -> bool:
+        """
+        Check if the key exists
+
+        :param key: key name
+        :type key: str
+
+        :return: True if the key exists, False otherwise
+        :rtype: bool
+        """
         return key in self._config
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
+        """
+        Iterate over the keys
+
+        :return: iterator over the keys
+        :rtype: Iterator[str]
+        """
         return iter(self._config)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Get the number of keys
+
+        :return: number of keys
+        :rtype: int
+        """
         return len(self._config)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """
+        Get the string representation of the config
+
+        :return: string representation of the config
+        :rtype: str
+        """
         return json.dumps(self._config, indent=4)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """
+        Get the string representation of the config
+
+        :return: string representation of the config
+        :rtype: str
+        """
         return f'Config({self.config_file})'
