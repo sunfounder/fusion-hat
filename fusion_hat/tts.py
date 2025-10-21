97eb8b6a5a60144bc31f5278dfa7f91cb1f2abe2
Cavon <cavonxx@gmail.com>
2025-10-21 16:46:37 +0800

    build: update dependency format and add docstrings to TTS classes
    Update the dependency format in pyproject.toml to use the correct pip syntax for git dependencies. Also add docstrings to all TTS classes in tts.py to improve code documentation.

--- fusion_hat/tts.py
+++ fusion_hat/tts.py
@@ -6,21 +6,25 @@ from sunfounder_voice_assistant.tts import OpenAI_TTS as _OpenAI_TTS
 from .utils import enable_speaker
 
 class Piper(_Piper):
+    """ Piper TTS class. """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         enable_speaker()
 
 class Pico2Wave(_Pico2Wave):
+    """ Pico2Wave TTS class. """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         enable_speaker()
 
 class Espeak(_Espeak):
+    """ Espeak TTS class. """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         enable_speaker()
 
 class OpenAI_TTS(_OpenAI_TTS):
+    """ OpenAI TTS class. """
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         enable_speaker()
