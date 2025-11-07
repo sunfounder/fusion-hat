cda5a7b5da7d241147b0f5cdc1064f6567a037df
kyuuinochi <52988090+kyuuinochi@users.noreply.github.com>
2025-11-07 17:48:57 +0800

    Update buzzer_test.py
    update buzzer test

--- examples/modules_examples/buzzer_test.py
+++ examples/modules_examples/buzzer_test.py
@@ -1,10 +1,38 @@
-# Import Buzzer class
-from fusion_hat import Buzzer, PWM
+#!/usr/bin/env python3
+from fusion_hat.modules import Buzzer
+from fusion_hat.pwm import PWM
+from time import sleep
 
-# Create Buzzer object
-buzzer = Buzzer(PWM(0))
+# Initialize a TonalBuzzer connected to PWM 0
+tb = Buzzer(PWM('P0'))  # Update this pin number based on your setup
 
-# Play a note
-buzzer.play("C0", 0.5)
+def play(tune):
+    """
+    Play a musical tune using the buzzer.
+    :param tune: List of tuples (note, duration), where each tuple represents a note and its duration.
+    """
+    for note, duration in tune:
+        print(note)  # Output the current note being played
+        tb.play(note,float(duration))  # Play the note on the buzzer
+    tb.off()  # Stop playing after the tune is complete
 
+# Define a musical tune as a sequence of notes and durations
+tune = [('C#4', 0.2), ('D4', 0.2), (None, 0.2),
+    ('Eb4', 0.2), ('E4', 0.2), (None, 0.6),
+    ('F#4', 0.2), ('G4', 0.2), (None, 0.6),
+    ('Eb4', 0.2), ('E4', 0.2), (None, 0.2),
+    ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
+    ('C4', 0.2), ('B4', 0.2), (None, 0.2),
+    ('F#4', 0.2), ('G4', 0.2), (None, 0.2),
+    ('B4', 0.2), ('Bb4', 0.5), (None, 0.6),
+    ('A4', 0.2), ('G4', 0.2), ('E4', 0.2), 
+    ('D4', 0.2), ('E4', 0.2)]
+
+
+try:
+    play(tune)  # Execute the play function to start playing the tune
+
+except KeyboardInterrupt:
+    # Handle KeyboardInterrupt for graceful termination
+    pass
 
