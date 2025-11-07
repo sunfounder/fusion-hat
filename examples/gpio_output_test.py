70820626b3824a1978ed5eb8c81b64d4e65226ae
kyuuinochi <52988090+kyuuinochi@users.noreply.github.com>
2025-11-07 17:52:16 +0800

    Update gpio_output_test.py

--- examples/gpio_output_test.py
+++ examples/gpio_output_test.py
@@ -1,10 +1,27 @@
-from fusion_hat.pin import Pin
+#!/usr/bin/env python3
+from fusion_hat.pin import Pin, Mode
 from time import sleep
 
-io17 = Pin(17, Pin.OUT)
+# Initialize an LED connected to GPIO pin 17 as an output pin.
+led = Pin(17,mode=Mode.OUT)
 
-while True:
-    io17.value(1)
-    sleep(0.5)
-    io17.value(0)
-    sleep(0.5)
+try:
+    # Start an infinite loop to toggle the LED state.
+    while True:
+        # Turn on the LED and print a message to the console.
+        led.high()
+        print('...LED ON')
+
+        # Wait for 0.5 seconds with the LED on.
+        sleep(0.5)
+
+        # Turn off the LED and print a message to the console.
+        led.low()
+        print('LED OFF...')
+
+        # Wait for 0.5 seconds with the LED off.
+        sleep(1)
+
+except KeyboardInterrupt:
+    # Gracefully handle a keyboard interrupt (Ctrl+C) by breaking the loop.
+    pass
