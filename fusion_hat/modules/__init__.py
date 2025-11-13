2a46d70b42908a59e7aaa40b8fddf288353a3668
susocool <susocool@outlook.com>
2025-11-13 11:41:24 +0800

    feat(modules): Add support for magnetometer and barometer modules
    -Add support for Magnetometer, qmc6310, qmc5883p, qmc5883l, hmc5883l magnetometer modules and BMP180 barometer module.

--- fusion_hat/modules/__init__.py
+++ fusion_hat/modules/__init__.py
@@ -10,3 +10,5 @@ from .dht11 import DHT11
 from .rotary_encoder import Rotary_Encoder
 from .rc522 import RC522
 from .led_matrix import LedMatrix
+from .magnetometers import Magnetometer, qmc6310, qmc5883p, qmc5883l, hmc5883l
+from .bmp180 import BMP180
